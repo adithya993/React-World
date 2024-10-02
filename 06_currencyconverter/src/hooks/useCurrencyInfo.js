e56031983f024ecaa8ced811cc0f1d1b237b3df1
adithya993 <adithya.rengarajan993@gmail.com>
2024-10-01 22:21:43 -0700

    react router dom portfolio work

--- 06_currencyconverter/src/hooks/useCurrencyInfo.js
+++ 06_currencyconverter/src/hooks/useCurrencyInfo.js
@@ -4,7 +4,6 @@ function useCurrencyInfo(currency){
     const[data, setData] = useState({});
     useEffect(() => {
          fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        //fetch(`https://api.github.com/users/hiteshchoudhary`)
             .then((res) => res.json())
             .then((res) => setData(res[currency]))
     }, [currency])
